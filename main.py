8d6a1449252fe16d4b77e1d26306acb06ef3b092
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 07:53:45 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class invoiceDyncWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = invoiceDyncWorker()
+    print("Worker engine initialized.")
